268ee26c4cb79d9c82c19fd9b0dc2b024402a0d6
MahekkumarChaniyara <mahekkumar.chaniyara@wotnot.io>
2025-10-15 11:58:16 +0530

    Initial commit: ChatBotSDK v1.0.0

---
+++ ChatBotSDK.xcframework/ios-arm64_x86_64-simulator/ChatBotSDK.framework/Headers/ChatBotSDK.h
@@ -0,0 +1,16 @@
+//
+//  ChatBotSDK.h
+//  ChatBotSDK
+//
+//  Created by Developer on 16/09/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatBotSDK.
+FOUNDATION_EXPORT double ChatBotSDKVersionNumber;
+
+//! Project version string for ChatBotSDK.
+FOUNDATION_EXPORT const unsigned char ChatBotSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatBotSDK/PublicHeader.h>
